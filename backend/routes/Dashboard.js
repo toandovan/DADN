d61851986f4f0b72fcad5205fc5deb3ff2bfb1cb
ngocphat <ngocphat.tin.hoa@gmail.com>
2020-07-10 17:55:34 +0700

    30 days data

--- backend/routes/Dashboard.js
+++ backend/routes/Dashboard.js
@@ -7,10 +7,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/date/:date/', function(req, res, next) {
   // console.log(req.params)
-  sensorMois.find()
+  let thirty_day_before = new Date(Date.now() - (30 * 24 * 60 * 60 * 1000));
+  sensorMois.find({ date: { $gt: thirty_day_before } } )
   // .where('date').gt(y).lt(x)
   .exec().then((doc)=>{
     // console.log(doc)
+    console.log(doc);
     res.status(200).json({
       // message: 'handling get request',
       sensorData: doc
